d9351e78fa708e420f13fd629adcd325cb37c82d
VINOD KUMAR <vinodkumar2050@gmail.com>
2024-05-03 16:46:16 +0000

    assdfgg

--- js_datatype.js
+++ js_datatype.js
@@ -4,7 +4,25 @@ string, number, boolean, undefined, null, symbol, bigint
 
 1 complex data type : object
 */
-let a=true;
+//let a=true;
 //console.log(a);c
-console.log(typeof(a));
+//console.log(typeof(a));
 
+let myName="foo"
+let myNumer=981589
+let myPass=false
+
+console.table([myName,myNumer,myPass])
+
+let myConst="like"
+console.log(myConst)
+myConst="whatsapp"
+console.log(myConst)
+let myId=null
+typeof(myId)
+console.log(typeof((myId)))
+
+let statement="name"
+console.log(statement)
+let statement=98756
+console.log(statement)
